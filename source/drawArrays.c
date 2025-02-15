29d2997b6a0a6e57ab1f550459224c293c113a2a
Wyatt-James <bomberbeast44@gmail.com>
2025-02-15 16:42:00 -0600

    Test GPUCMD_BatchOfSingles - just for drawArrays.c for now.

--- source/drawArrays.c
+++ source/drawArrays.c
@@ -6,28 +6,32 @@ void C3D_DrawArrays(GPU_Primitive_t primitive, int first, int size)
 
 	C3Di_Profile_Enter_Block(C3D_ProfilerSlot_DrawArrays);
 
-	// Set primitive type
-	GPUCMD_AddMaskedWrite(GPUREG_PRIMITIVE_CONFIG, 2, primitive);
-	// Start a new primitive (breaks off a triangle strip/fan)
-	GPUCMD_AddWrite(GPUREG_RESTART_PRIMITIVE, 1);
-	// The index buffer is not used, but this command is still required
-	GPUCMD_AddWrite(GPUREG_INDEXBUFFER_CONFIG, 0x80000000);
-	// Number of vertices
-	GPUCMD_AddWrite(GPUREG_NUMVERTICES, size);
-	// First vertex
-	GPUCMD_AddWrite(GPUREG_VERTEX_OFFSET, first);
-	// Enable array drawing mode
-	GPUCMD_AddMaskedWrite(GPUREG_GEOSTAGE_CONFIG2, 1, 1);
-	// Enable drawing mode
-	GPUCMD_AddMaskedWrite(GPUREG_START_DRAW_FUNC0, 1, 0);
-	// Trigger array drawing
-	GPUCMD_AddWrite(GPUREG_DRAWARRAYS, 1);
-	// Go back to configuration mode
-	GPUCMD_AddMaskedWrite(GPUREG_START_DRAW_FUNC0, 1, 1);
-	// Disable array drawing mode
-	GPUCMD_AddMaskedWrite(GPUREG_GEOSTAGE_CONFIG2, 1, 0);
-	// Clear the post-vertex cache
-	GPUCMD_AddWrite(GPUREG_VTX_FUNC, 1);
+	gpucmd_single_t arr[] = {
+		// Set primitive type
+		GPUCMD_MaskedSingle(GPUREG_PRIMITIVE_CONFIG, 2, primitive),
+		// Start a new primitive (breaks off a triangle strip/fan)
+		GPUCMD_Single(GPUREG_RESTART_PRIMITIVE, 1),
+		// The index buffer is not used, but this command is still required
+		GPUCMD_Single(GPUREG_INDEXBUFFER_CONFIG, 0x80000000),
+		// Number of vertices
+		GPUCMD_Single(GPUREG_NUMVERTICES, size),
+		// First vertex
+		GPUCMD_Single(GPUREG_VERTEX_OFFSET, first),
+		// Enable array drawing mode
+		GPUCMD_MaskedSingle(GPUREG_GEOSTAGE_CONFIG2, 1, 1),
+		// Enable drawing mode
+		GPUCMD_MaskedSingle(GPUREG_START_DRAW_FUNC0, 1, 0),
+		// Trigger array drawing
+		GPUCMD_Single(GPUREG_DRAWARRAYS, 1),
+		// Go back to configuration mode
+		GPUCMD_MaskedSingle(GPUREG_START_DRAW_FUNC0, 1, 1),
+		// Disable array drawing mode
+		GPUCMD_MaskedSingle(GPUREG_GEOSTAGE_CONFIG2, 1, 0),
+		// Clear the post-vertex cache
+		GPUCMD_Single(GPUREG_VTX_FUNC, 1),
+	};
+
+	GPUCMD_AddBatchOfSingles(arr);
 
 	C3Di_GetContext()->flags |= C3DiF_DrawUsed;
 
